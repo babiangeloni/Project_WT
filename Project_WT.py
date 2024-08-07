9591c3c30109ea4d195dd50feba1c4c0996a7b99
babiangeloni <158472029+babiangeloni@users.noreply.github.com>
2024-08-07 13:11:56 +0200

    Update Project_WT.py

--- Project_WT.py
+++ Project_WT.py
@@ -8,9 +8,7 @@ from PIL import Image
 def display_header():
     # Display the cover image
     image = Image.open("Surface world Temperature Analysis.png")
-    # Visualizza l'immagine in Streamlit
     st.image(image)
-
 
     # Caption with course and group information
     st.caption("""**Course** : Data Analyst
@@ -21,7 +19,7 @@ def display_header():
 
 # Function for the introduction page
 def introduction():
-    display_header()  # Call the function to display the header content
+    display_header()  
 
     st.title("👋 Introduction")
     st.info("This study aims to leverage machine learning models to predict surface temperature anomalies by identifying key variables that influence temperature changes, ultimately contributing to more informed climate-related decisions.")
@@ -40,9 +38,9 @@ def introduction():
     """, unsafe_allow_html=True)
 
 def data_exploration():
-    display_header()  # Call the function to display the header content
+    display_header()  
     st.title("🔎 Data Exploration")
-    st.info("To achieve the objectives of this project, we used 3 main datasets, each providing unique insights into different aspects of global warming and CO₂ emissions and we performed a Data Audit of each of them.")
+    st.info("To achieve the objectives of this project, we utilized three primary datasets. Below, we present key insights derived from these datasets, covering various aspects of global warming, CO₂ emissions, and the results of our data audit.")
     
     # DF1
     """This project utilizes three main datasets:"""
@@ -54,8 +52,9 @@ def data_exploration():
                 - **Interesting variables**: different sources from CO2, temperature change, population, gdp
                 - **Missing values**: more than 50% of the columns of the original dataset presented missing values
                 """
-            
-            """Preview of the OWID CO2 Data File:"""
+            """
+                Preview of the OWID CO2 Data File: 
+            """
             image = Image.open("C:\\Users\\gabri\\Desktop\\Project_WT\\Project_WT\\images\\DF1 HEAD.png")
             st.image(image)
 
@@ -67,8 +66,9 @@ def data_exploration():
                 - **Missing values**: There is no missing values for surface temperature anomaly data
                 """
 
-                """Preview of the HADCRUT Surface Temperature Anomaly File:"""
-
+                """
+                Preview of the HADCRUT Surface Temperature Anomaly File:
+                """
                 image = Image.open("C:\\Users\\gabri\\Desktop\\Project_WT\\Project_WT\\images\\DF2 HEAD.png")
                 st.image(image)
 
@@ -78,74 +78,19 @@ def data_exploration():
                 - **Volume**: 285 rows and 4 columns
                 - **Description**: This dataset provided information about countries and continents, with the year value being 2015
                 - **Missing values**: The dataset included 285 countries with no missing values
-
                 """
-
-                """Preview of the Our World in Data: Continents File:"""
+                """
+                Preview of the Our World in Data: Continents File:
+                """
 
                 image = Image.open("C:\\Users\\gabri\\Desktop\\Project_WT\\Project_WT\\images\\DF3 HEAD.png")
                 st.image(image)
 
-##### PREDICTION SECTION
-
-# Function to load the trained model
-def load_model():
-    with open('model.pkl', 'rb') as model_file:
-        model = pickle.load(model_file)
-    return model
-
-# Function to create an array of features for prediction
-def get_features(continent, country, year, population, temperature_change_tot, temperature_change_from_ch4, temperature_change_from_co2, temperature_change_from_ghg, temperature_change_from_n2o, co2):
-    features = np.array([
-        continent, country, year, population, temperature_change_tot,
-        temperature_change_from_ch4, temperature_change_from_co2,
-        temperature_change_from_ghg, temperature_change_from_n2o, co2
-    ])
-    return features.reshape(1, -1)
-
-# Function to make a prediction using the loaded model
-def predict_surface_temperature(features):
-    model = load_model()
-    prediction = model.predict(features)
-    return np.round(prediction, 3)
-
-
-# Streamlit UI for the prediction page
-def prediction():
-    display_header()  # Call the function to display the header content
-
-    st.title("🔮 Prediction")
-    st.write("The Random Forest is an ensemble learning method that combines multiple decision trees to improve the model’s accuracy and robustness. Each tree in the forest is built from a random subset of the data and features, and the final prediction is made by averaging the predictions of all individual trees.")
-    st.info("Our model was fine-tuned using GridSearchCV to optimise its hyperparameters and improve performance.")
-
-    # Sliders to capture input values for each feature
-    continent = st.slider('Continent', min_value=0.0, max_value=5.0, value=0.0, step=1.0)
-    country = st.slider('Country', min_value=0.0, max_value=190.0, value=0.0, step=1.0)
-    year = st.slider('Year', min_value=1851, max_value=2017, value=2020, step=1)  # Treated as an integer
-    population = st.slider('Population', min_value=3187.0, max_value=1410275968.0, value=1000000.0)
-    temperature_change_tot = st.slider('Total Temperature Change', min_value=-0.002, max_value=0.541, value=0.0)
-    temperature_change_from_ch4 = st.slider('Temperature Change from CH4', min_value=-0.001, max_value=0.056, value=0.0)
-    temperature_change_from_co2 = st.slider('Temperature Change from CO2', min_value=0.0, max_value=0.225, value=0.0)
-    temperature_change_from_ghg = st.slider('Temperature Change from GHG', min_value=-0.001, max_value=0.271, value=0.0)
-    temperature_change_from_n2o = st.slider('Temperature Change from N2O', min_value=0.0, max_value=0.01, value=0.0)
-    co2 = st.slider('CO2', min_value=0.0, max_value=10011.151, value=0.0)
-
-    # Generate prediction when the button is clicked
-    if st.button('Predict Surface Temperature'):
-        features = get_features(continent, country, year, population, temperature_change_tot, temperature_change_from_ch4, temperature_change_from_co2, temperature_change_from_ghg, temperature_change_from_n2o, co2)
-        prediction = predict_surface_temperature(features)
-        st.write(f"The predicted surface temperature is: {prediction[0]}°C")
-
-
-
-####### PREDICTION
-
-# Placeholder functions for other pages
 
 def visualization():
-    display_header()  # Call the function to display the header content
+    display_header()  
     st.title("🌏 Visualization")
-    st.write("Visualization content goes here.")
+    st.write("In the Visualization section, we present a series of graphics that offer a visual interpretation of the data, helping to illuminate key trends and patterns. These visuals provide a deeper understanding of the our dataset.")
 
     st.title("Surface temperature anomaly variation over the years (globally)")
     image = Image.open("C:\\Users\\gabri\\Desktop\\Project_WT\\Project_WT\\images\\Surface temperature anomaly variation over the years (globally).png")
@@ -211,6 +156,7 @@ def modeling():
     display_header()  # Call the function to display the header content
     st.title("🧩 Modelling")
     st.info("Our goal: to identify the best performing machine learning model for predicting surface temperature anomalies using various regression techniques.")
+
     st.write("### Methodology")
     st.write("- First check of the missing values and modalities of our merged dataset")
     st.write("- Encoded the categorical variables with get_dummies")
@@ -221,58 +167,118 @@ def modeling():
     st.write("### Results")
     st.write("In the table below you can find the results from the different Model presented: ")
     
-    if st.button("Metrics performance") :
+    with st.expander("Metrics performance") :
+        st.title("Best performing model: Random Forest Regressor")
         image_mod = Image.open("model_performance_evaluation.png")
         st.image(image_mod)
-
-    st.write("### Best performing model: Random Forest Regressor")
-    
-
-    st.write("- In terms of **Feature Importances** for the Random Forest, as showed in the chart, the temperature-related features have very low importance, indicating that the model relies primarily on temporal and demographic data rather than temperature change metrics.")
-    if st.button("Feature Importances") :
+        st.write("- R²: The model achieves an R² score of 71.77%, indicating it explains a significant portion of the variance. When applied to the test data, the R² score drops to 52.33%, suggesting a decrease in performance and potential overfitting, as previously explained.")
+        st.write("- MAE: indicating the average deviation of the model's predictions from the actual surface temperature anomalies is 0.3233")
+        st.write("- MSE for the test data is 0.2046, representing the average of the squared differences between predicted and actual values, highlighting the impact of larger errors on the model’s performance.")
+        st.write("- RMSE for the test set is 0.4524, indicating the standard deviation of the prediction errors and showing how far the model’s predictions deviate from the actual values on average, providing a sense of accuracy relative to the target variable's units.")
+   
+    with st.expander("Feature Importances") :
+        st.title("Feature Importances")
         image_fi = Image.open("feat_imp.png")
         st.image(image_fi)
+        st.write("In terms of **Feature Importances** for the Random Forest, the temperature-related features have very low importance, indicating that the model relies primarily on temporal and demographic data rather than temperature change metrics.")
     
-    st.write("- The **Scatter plot Target Vs. Residuals** shows that the Random Forest model generally performs well, with many predictions closely aligning with actual values, but there are still challenges with accurately predicting the extremes.")
-    if st.button("Scatter Plot Scatter vs. Prediction") :
+    with st.expander("Scatter Plot") :
+        st.title("Scatter Plot Scatter vs. Prediction")
         image_tp = Image.open("target_pred.png")
         st.image(image_tp)
-   
-    st.write("- As the **Scatter Plot of Residuals** suggests that the Random Forest model fits the data well, with consistent prediction errors across the range of values.")
-    if st.button("Scatter Plot of Residuals") :
+        st.write("The **Scatter Plot of Residuals** suggests that the Random Forest model fits the data well, with consistent prediction errors across the range of values, but there are still challenges with accurately predicting the extremes.")
+    
+    with st.expander("Residuals") :
+        st.title("Scatter Plot of Residuals")
         image_rsp = Image.open("scatter_plot.png")
         st.image(image_rsp)
-
-    st.write("- The **Histogram of Residuals** confirms that the Random Forest model performs well, with a good distribution of residuals around zero.")
-    if st.button("Histogram of Residuals") :
+    
+        st.title("Histogram of Residuals")
         image_hr = Image.open("hist_res.png")
         st.image(image_hr)
-    
-    st.info("Based on the provided metrics and analysis, the **Random Forest Regressor** was the best-performing model for predicting surface temperature anomaly in this dataset, therefore is the one that we selected to build our Machine Learning model to predict the surface temperature anomaly.")
- 
+        
+        st.write("- The **Histogram of Residuals** confirms that the Random Forest model performs well, with a good distribution of residuals around zero.")
 
+    st.info("Based on the provided metrics and analysis, the **Random Forest Regressor** was the best-performing model for predicting surface temperature anomaly in this dataset, therefore is the one that we selected to build our Machine Learning model to predict the surface temperature anomaly. Below it is possible to see the results also of some visual analysis about the Feature Importances and Residuals")
+
+
+
+##### PREDICTION SECTION
+
+# Function to load the trained model
+def load_model():
+    with open('model.pkl', 'rb') as model_file:
+        model = pickle.load(model_file)
+    return model
+
+# Function to create an array of features for prediction
+def get_features(continent, country, year, population, temperature_change_tot, temperature_change_from_ch4, temperature_change_from_co2, temperature_change_from_ghg, temperature_change_from_n2o, co2):
+    features = np.array([
+        continent, country, year, population, temperature_change_tot,
+        temperature_change_from_ch4, temperature_change_from_co2,
+        temperature_change_from_ghg, temperature_change_from_n2o, co2
+    ])
+    return features.reshape(1, -1)
+
+# Function to make a prediction using the loaded model
+def predict_surface_temperature(features):
+    model = load_model()
+    prediction = model.predict(features)
+    return np.round(prediction, 3)
+
+
+# Streamlit UI for the prediction page
+def prediction():
+    display_header()  # Call the function to display the header content
+
+    st.title("🔮 Prediction")
+    st.write("The Random Forest is an ensemble learning method that combines multiple decision trees to improve the model’s accuracy and robustness. Each tree in the forest is built from a random subset of the data and features, and the final prediction is made by averaging the predictions of all individual trees.")
+    st.info("Our model was fine-tuned using GridSearchCV to optimise its hyperparameters and improve performance.")
+
+    # Sliders to capture input values for each feature
+    continent = st.slider('Continent', min_value=0.0, max_value=5.0, value=0.0, step=1.0)
+    country = st.slider('Country', min_value=0.0, max_value=190.0, value=0.0, step=1.0)
+    year = st.slider('Year', min_value=1851, max_value=2017, value=2020, step=1)  # Treated as an integer
+    population = st.slider('Population', min_value=3187.0, max_value=1410275968.0, value=1000000.0)
+    temperature_change_tot = st.slider('Total Temperature Change', min_value=-0.002, max_value=0.541, value=0.0)
+    temperature_change_from_ch4 = st.slider('Temperature Change from CH4', min_value=-0.001, max_value=0.056, value=0.0)
+    temperature_change_from_co2 = st.slider('Temperature Change from CO2', min_value=0.0, max_value=0.225, value=0.0)
+    temperature_change_from_ghg = st.slider('Temperature Change from GHG', min_value=-0.001, max_value=0.271, value=0.0)
+    temperature_change_from_n2o = st.slider('Temperature Change from N2O', min_value=0.0, max_value=0.01, value=0.0)
+    co2 = st.slider('CO2', min_value=0.0, max_value=10011.151, value=0.0)
+
+    # Generate prediction when the button is clicked
+    if st.button('Predict Surface Temperature'):
+        features = get_features(continent, country, year, population, temperature_change_tot, temperature_change_from_ch4, temperature_change_from_co2, temperature_change_from_ghg, temperature_change_from_n2o, co2)
+        prediction = predict_surface_temperature(features)
+        st.write(f"The predicted surface temperature is: {prediction[0]}°C")
+
+
+
+####### PREDICTION
 
 def conclusion():
-    display_header()  # Call the function to display the header content
-    st.title("📌 Conclusion")
-    st.info("In conclusion, we aimed to develop a predictive model for surface temperature anomalies using various datasets and machine learning techniques. Among the models tested, the Random Forest emerged as the most effective for predicting surface temperature anomalies.")
+   display_header()  # Call the function to display the header content
+   st.title("📌 Conclusion")
+   st.info("In conclusion, we aimed to develop a predictive model for surface temperature anomalies using various datasets and machine learning techniques. Among the models tested, the Random Forest emerged as the most effective for predicting surface temperature anomalies.")
 
-    st.write("### Challenges")
-    st.write("- Numerous datasets to analyze, with some containing overlapping or repetitive information")
-    st.write("- Data merging presented initial difficulties, especially in regards to the selections of the variables to include and the best strategy to handle missing values.")
-    st.write("### Recommandations")
-    st.write("- **Data Selection**: Careful consideration should be given to the selection of datasets, ensuring that redundant or overlapping data is minimized.")
-    st.write("- **Inclusion of Additional Variables**: Future efforts could focus on identifying and incorporating additional relevant variables to enhance the predictive power of the models.")
-    st.write("- **Merging Strategy**: A more robust strategy for merging datasets could help in overcoming initial challenges and optimizing the data for better model performance.")
+   st.write("### Challenges")
+   st.write("- **Time Management**: The challenge was balancing the demands of the project with our ongoing learning process. As junior members, we struggled to manage our time effectively, especially when applying newly acquired theoretical knowledge to practical scenarios.")
+   st.write("- **Data Merging**: Merging datasets proved difficult, particularly in selecting which variables to include and in handling missing values. This was complicated by overlapping or repetitive information in some of the datasets.")
+   st.write("- **Model Performance**: Despite achieving an R² score of 71.77% on the training data, the model's performance dropped significantly on the test data, with an R² score of 52.33%. This suggests potential overfitting and indicates that the model struggled to generalize to new, unseen data.")
+   
+   st.write("### Learnings and Recommendations")
+   st.write("- **Time Management**: A solution could have been establishing a more structured timeline with specific milestones to better distribute tasks and reduce last-minute pressures. Improved planning would have helped in balancing the project workload with ongoing learning.")
+   st.write("- **Data Merging**: A more robust strategy for merging datasets could have alleviated initial challenges. This includes careful consideration of variable selection, as well as better handling of missing values to optimize data quality and improve model performance.")
+   st.write("- **Model Performance**: Incorporating more relevant features could have enhanced the model’s ability to generalize. Implementing more robust cross-validation techniques might also have provided a better assessment of the model's performance on unseen data, helping to improve its generalization capabilities.")
 
-
-    st.markdown("""
-    <style>
-        .right-align {
-            text-align: right;
-        }
-    </style>
-    """, unsafe_allow_html=True)
+   st.markdown("""
+   <style>
+       .right-align {
+           text-align: right;
+       }
+   </style>
+   """, unsafe_allow_html=True)
 
 # Main function for navigation
 def main():
