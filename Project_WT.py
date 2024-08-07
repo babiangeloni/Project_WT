352abe26f9e741c530644d48c19b1e561953fbbf
babiangeloni <158472029+babiangeloni@users.noreply.github.com>
2024-08-07 11:53:09 +0200

    Update Project_WT.py

--- Project_WT.py
+++ Project_WT.py
@@ -2,8 +2,6 @@ import streamlit as st
 import pandas as pd 
 import pickle
 import numpy as np
-import plotly.graph_objects as go
-import plotly.express as px
 from io import StringIO
 import requests
 from io import BytesIO
